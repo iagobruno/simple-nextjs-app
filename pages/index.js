bd8984e8a6c7678e47bc50bc24d4f0345dd2a3b1
httpiago <httpiago@gmail.com>
2018-06-28 13:32:46 -0300

    improve index page

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,30 @@
-export default () => <div>Welcome to next.js!</div>
+import React, { Component } from 'react'
+
+class Home extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handleClick = this.handleClick.bind(this)
+  }
+  state = {
+    title: 'hey'
+  }
+
+  handleClick(event) {
+    this.setState((prevState) => {
+      return {
+        title: prevState.title + ' hey'
+      }
+    })
+  }
+
+  render() { 
+    return (
+      <div>
+        <h2 onClick={this.handleClick}>{this.state.title}</h2>
+      </div>
+    )
+  }
+}
+ 
+export default Home
