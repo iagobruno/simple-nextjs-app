5c50d2589e44373ebd0d2d4b5c41cd4310c73b76
Iago Bruno <httpiago@gmail.com>
2018-08-09 04:27:29 -0300

    Mapear as páginas durante a exportação

--- next.config.js
+++ next.config.js
@@ -2,4 +2,11 @@ const withLess = require('@zeit/next-less')
 const withCSS = require('@zeit/next-css')
 const withImages = require('next-images')
 
-module.exports = withImages(withCSS(withLess()))
+module.exports = withImages(withCSS(withLess({
+  exportPathMap: async function (defaultPathMap) {
+    return {
+      '/': { page: '/index' },
+      '/about': { page: '/about', query: { username: 'httpiago' } }
+    }
+  }
+})))
