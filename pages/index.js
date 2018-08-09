51587fad2a999acb05739e94cc4734ee9771ea56
Iago Bruno <httpiago@gmail.com>
2018-08-09 06:57:01 -0300

    Animação legal ao iniciar a página

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,27 @@ class Home extends Component {
   state = {
     githubUsername: 'httpiago'
   }
+	
+  componentDidMount = () => {
+    // Fazer uma animação legal quando a página carregar
+    let elements = ['.title', '.description', '.inputUsername', '.btn']
+    let i = 0
 
+	  // Fazer um loop nos elementos com um delay de diferença entre cada um
+    let timer = setInterval(function() {
+
+      // Mostrar o elemento
+      document.getElementsByClassName(elements[i].slice(1))[0].classList.remove('hide')
+
+      // Parar o timer quando chegar no último elemento
+      if (i >= elements.length - 1) clearInterval(timer)
+      else i++
+
+    }, 100)
+
+    // Alinhar a página 
+    document.getElementsByClassName('logoHP')[0].style.marginTop = ''
+  }
   showRepositories = (event) => {
     event.preventDefault()
 
@@ -24,14 +44,14 @@ class Home extends Component {
   render() { 
     return (
       <Page>
-        <img className="logoHP" src="/static/logo.svg" />
-        <h1 className="title">Hello world!</h1>
-        <div className="description">
+        <img className="logoHP" src="/static/logo.svg" style={{marginTop:250}} />
+        <h1 className="title hide">Hello world!</h1>
+        <div className="description hide">
           Esse aplicativo lista todos os{' '}
           <Link href={`https://github.com/${this.state.githubUsername}`}><a>seus repositórios do GitHub</a></Link>{' '}
           (sem contar com os forks).
         </div>
-        <Form onSubmit={this.showRepositories}>
+        <Form id="username-form" onSubmit={this.showRepositories}>
           {/* Salvar o valor do formulário no navegador */}
           <Persist 
             name="username-form" 
@@ -39,8 +59,8 @@ class Home extends Component {
             debounce={300} 
             onMount={(data) => this.setState(data)}
           />
-          <Input type="text" className="inputUsername" placeholder={`Username do GitHub (${this.state.githubUsername})`} onChange={this.handleInputChange} />
-          <Button color="primary" onClick={this.showRepositories}>Listar repositórios</Button>
+          <Input type="text" className="inputUsername hide" placeholder={`Username do GitHub (${this.state.githubUsername})`} onChange={this.handleInputChange} />
+          <Button color="primary" className="hide" onClick={this.showRepositories}>Listar repositórios</Button>
         </Form>
       </Page>
     )
